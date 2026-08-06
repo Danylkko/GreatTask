d87de41f4df445e4d7c8f9cd36a27a57e1fdf9ee
Danylo Litvinchuk <ltvnchkd@gmail.com>
2026-08-06 15:52:03 +0200

    Initial Commit

---
+++ GreatTask/GreatTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreatTaskApp.swift
+//  GreatTask
+//
+//  Created by Danylo Litvinchuk on 06.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct GreatTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
